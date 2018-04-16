e47062a4b439d49a4d46fc35b9c6f8b8cdda4e04
George Kolecsanyi <gyuri.kol@gmail.com>
2018-04-16 22:19:48 +1000

    First Commit

---
+++ build/Pods.build/Debug-iphoneos/Pods-Speech.build/DerivedSources/Pods_Speech_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_SpeechVersionString[];
+ extern const double Pods_SpeechVersionNumber;
+
+ const unsigned char Pods_SpeechVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Speech  PROJECT:Pods-1" "\n";
+ const double Pods_SpeechVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/Pods.build/Debug-iphoneos/googleapis.build/DerivedSources/googleapis_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char googleapisVersionString[];
+ extern const double googleapisVersionNumber;
+
+ const unsigned char googleapisVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:googleapis  PROJECT:Pods-1" "\n";
+ const double googleapisVersionNumber __attribute__ ((used)) = (double)1.;

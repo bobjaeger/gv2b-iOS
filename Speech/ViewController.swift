e548348088d08798497a263bb01372049b66d682
George Kolecsanyi <gyuri.kol@gmail.com>
2018-05-08 12:48:58 +1000

    changes

--- Speech/ViewController.swift
+++ Speech/ViewController.swift
@@ -16,7 +16,6 @@
 import UIKit
 import AVFoundation
 import googleapis
-import QuartzCore
 
 let SAMPLE_RATE = 16000
 
@@ -57,12 +56,14 @@ class ViewController : UIViewController, AudioControllerDelegate {
   @IBOutlet weak var micStart: UIButton!
   @IBOutlet weak var micStop: UIButton!
   @IBOutlet weak var transcriptSpace: UIView!
-  @IBOutlet weak var eventList: UIView!
+  @IBOutlet weak var eventList: UIScrollView!
   @IBOutlet weak var currentBidView: UIView!
   @IBOutlet weak var currentBidLabel: UILabel!
   @IBOutlet weak var menuView: UIView!
     
   var audioData: NSMutableData!
+    
+  var uiViewEventArray: [UIView] = []
     
   let speechOnColor = UIColor(red:0.00, green:0.83, blue:0.78, alpha:1.0)
   let speechOffColor = UIColor(red:0.15, green:0.15, blue:0.15, alpha:1.0)
@@ -97,6 +98,9 @@ class ViewController : UIViewController, AudioControllerDelegate {
     
     micStart.isHidden = true
     micStop.isHidden = false
+    
+    // Scroll to bottom of scroll view
+    eventList.scrollTo(direction: .Bottom, animated: true)
   }
 
   @IBAction func stopAudio(_ sender: NSObject) {
@@ -107,6 +111,9 @@ class ViewController : UIViewController, AudioControllerDelegate {
     
     micStart.isHidden = false
     micStop.isHidden = true
+    
+    // Scroll to bottom of scroll view
+    eventList.scrollTo(direction: .Bottom, animated: true)
   }
     
     // function to HIDE transcript area and text view with animation
@@ -183,7 +190,51 @@ class ViewController : UIViewController, AudioControllerDelegate {
             })
         })
     }
-
+    
+    // add event or bubble to final transcription
+    func addFinalTranscription(transcription: String) {
+        let addedView = UIView()
+        addedView.frame = CGRect(
+            origin: CGPoint(x: 0,y : Int(eventList.frame.height) + (50 * uiViewEventArray.count) ),
+            size: CGSize(
+                width: (self.eventList.frame.width),
+                height: 50)
+        )
+        let transcriptLabel = UILabel()
+        transcriptLabel.frame = CGRect(
+            origin: CGPoint(x: 15,y : 0),
+            size: CGSize(
+                width: ((self.eventList.frame.width) - 20),
+                height: 40)
+        )
+        transcriptLabel.center = CGPoint(x: addedView.frame.width/2, y: addedView.frame.height/2)
+        transcriptLabel.font.withSize(6.0)
+        transcriptLabel.backgroundColor = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+        transcriptLabel.layer.masksToBounds = true
+        transcriptLabel.layer.cornerRadius = 20
+        transcriptLabel.text = transcription
+        transcriptLabel.textAlignment = NSTextAlignment.right
+        addedView.addSubview(transcriptLabel)
+        
+        // add to uiViewArray
+        uiViewEventArray.append(addedView)
+        eventList.contentSize = CGSize(
+            width: eventList.frame.width,
+            height: (
+                CGFloat(
+                    Float(eventList.frame.height) + (50.0 * Float(uiViewEventArray.count) )
+                )
+            )
+        )
+        self.eventList.addSubview(addedView)
+        
+        // Scroll to bottom of scroll view
+        eventList.scrollTo(direction: .Bottom, animated: true)
+        
+        // set transcription field to empty string
+        textView.text = String()
+    }
+    
   func processSampleData(_ data: Data) -> Void {
     audioData.append(data)
 
@@ -204,6 +255,7 @@ class ViewController : UIViewController, AudioControllerDelegate {
                 strongSelf.textView.text = error.localizedDescription
             } else if let response = response {
                 var finished = false    // flag to identify end of speech recognition result
+                print(response)
                 
                 // if result or google api return is a streaming recognition result
                 for result in response.resultsArray! {
@@ -215,6 +267,7 @@ class ViewController : UIViewController, AudioControllerDelegate {
                             // set recognition to temporary variable
                             self?.tempResult = result
                             stable = true
+                            strongSelf.textView.text = String()
                         } else if (self?.tempResult.stability)! <= result.stability {
                             // otherwise if stability of result is equal or better then temporary variable
                             // replace temp variable with more stable option
@@ -236,7 +289,10 @@ class ViewController : UIViewController, AudioControllerDelegate {
                             
                             // print the running transcript
                             if let resultFirstAlt = result.alternativesArray.firstObject as? SpeechRecognitionAlternative {
-                                strongSelf.textView.text = resultFirstAlt.transcript  // add transcript to textView
+                                strongSelf.textView.text = resultFirstAlt.transcript
+                                
+                                // add event to event list
+                                self?.addFinalTranscription(transcription: resultFirstAlt.transcript)
                             }
                             
                             // reset temp result
